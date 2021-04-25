6a98d8cc8df38bd1c85172a9ca7028d36f38741f
Dario Velez Roman <dvelezroman@gmail.com>
2021-04-24 22:02:26 -0300

    add buzzer

--- utils/door.js
+++ utils/door.js
@@ -1,13 +1,16 @@
 const { Gpio } = require('onoff')
 const DOOR = new Gpio(4, 'out')
+const BUZZER = new Gpio(17, 'out')
 
 function closeDoor() {
   DOOR.writeSync(1)
+  BUZZER.writeSync(0)
 }
 
 function openDoor(ms) {
   if (DOOR.readSync() === 1) {
     DOOR.writeSync(0)
+    BUZZER.writeSync(1)
     setTimeout(closeDoor, ms);
   }
 }
